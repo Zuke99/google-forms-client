dd1355e70a189a59740ee7b27ab1caec6b50beca
Dheeraj P <dheerajp@Dheerajs-MacBook-Pro.local>
2023-09-18 22:12:02 +0530

    Added TailwindCSS

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -4,7 +4,7 @@ import jwt_decode from "jwt-decode";
 function GoogleAuth() {
   return (
     <div>
-      Google AUTH
+      <h1 className='border font-extrabold text-3xl'>Google AUTH</h1>
       <GoogleOAuthProvider clientId='947186499507-8akbhmfsnggvf1ju4nuom93grn8iaoil.apps.googleusercontent.com'>
         <GoogleLogin onSuccess={(credentialResponse) => {
             var decoded = jwt_decode(credentialResponse.credential);
